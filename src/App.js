079e4fe0fddc41affad7770154fbf15cd3f4b89f
Valeria Castillo <valeriacastillo415@gmail.com>
2021-06-06 11:04:47 -0700

    Created Header, Tasks, Btn, Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Drs appt",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Dentist appt",
+      day: "Feb 6th at 2:00pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Training Session",
+      day: "Feb 3th at 1:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks Available'}
     </div>
   );
 }
